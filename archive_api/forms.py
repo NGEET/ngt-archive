e528ece6de47936db5e4c65c6c017d6e930983fd
Val Hendrix <vchendrix@lbl.gov>
2021-11-30 14:29:39 -0800

    Updates Metric Page text
    + Provides help text and more descriptive buttons lables
      for metrics page.
    
    Issue #344

--- archive_api/forms.py
+++ archive_api/forms.py
@@ -6,10 +6,11 @@ from django.utils import timezone
 
 class MetricsFilterForm(forms.Form):
     start_date = forms.DateField(label='Start Date',
-                                 widget=forms.DateInput(attrs={'maxlength': 10}), initial=date(year=2016, month=1, day=1),
-                                 required=True)
-    end_date = forms.DateField(label='End Date', widget=forms.DateInput(attrs={'maxlength': 10}), initial=timezone.now(),
-                               required=True)
-
-
-
+                                 widget=forms.DateInput(attrs={'maxlength': 10}),
+                                 initial=date(year=2016, month=1, day=1),
+                                 required=True,
+                                 help_text="Filter the metrics on or after this date.")
+    end_date = forms.DateField(label='End Date', widget=forms.DateInput(attrs={'maxlength': 10}),
+                               initial=timezone.now(),
+                               required=True,
+                               help_text="Filter metrics before this date.")
